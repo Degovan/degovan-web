dcba958ae35481f230a9caf82171b101c278a059
Mohamad Hafid Masruri <do.crazy192@gmail.com>
2020-11-21 22:06:50 +0700

    create style include

---
+++ resources/views/admin/layouts/script.blade.php
@@ -0,0 +1,39 @@
+<!-- Core -->
+<script src="{{ asset('assets/vendor/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+<!-- Vendor JS -->
+<script src="{{ asset('assets/vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
+
+<!-- Slider -->
+<script src="{{ asset('assets/vendor/nouislider/distribute/nouislider.min.js') }}"></script>
+
+<!-- Jarallax -->
+<script src="{{ asset('assets/vendor/jarallax/dist/jarallax.min.js') }}"></script>
+
+<!-- Smooth scroll -->
+<script src="{{ asset('assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+
+<!-- Count up -->
+<script src="{{ asset('assets/vendor/countup.js/dist/countUp.umd.js') }}"></script>
+
+<!-- Notyf -->
+<script src="{{ asset('assets/vendor/notyf/notyf.min.js') }}"></script>
+
+<!-- Charts -->
+<script src="{{ asset('assets/vendor/chartist/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+
+<!-- Datepicker -->
+<script src="{{ asset('assets/vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+<!-- Simplebar -->
+<script src="{{ asset('assets/vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js')"></script>
+
+<!-- Volt JS -->
+<script src="{{ asset('assets/js/volt.js') }}"></script>
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
