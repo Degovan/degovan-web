fe229d125d35dec77be18dbb18930fab829ab9f3
Hafet17 <do.crazy192@gmail.com>
2020-11-21 22:28:38 +0700

    memindah controllers ke admin

---
+++ app/Http/Controllers/Admin/PortofolioController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Portofolio;
+
+class PortofolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Portofolio  $portofolio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Portofolio $portofolio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Portofolio  $portofolio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Portofolio $portofolio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Portofolio  $portofolio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Portofolio $portofolio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Portofolio  $portofolio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Portofolio $portofolio)
+    {
+        //
+    }
+}
