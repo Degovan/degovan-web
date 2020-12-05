56abd47d03ab064c07f83e6c10e70e99f6d4e5fd
Hafet17 <do.crazy192@gmail.com>
2020-12-05 17:39:42 +0700

    create user index views

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,98 @@
+@extends('admin.layouts.app',['title' => 'User'])
+
+@section('content')
+
+<div class="py-4">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item"><a href="#"><span class="fas fa-home"></span></a></li>
+            <li class="breadcrumb-item"><a href="#">User</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Data User</li>
+        </ol>
+    </nav>
+
+</div>
+
+<div class="row">
+
+    @if (session('status'))
+    @push('addon-script')
+    <script>
+        swal({
+            title: "Good job!",
+            text: "{{ session('status') }}",
+            icon: "success",
+            button: false,
+            timer: 3000
+        });
+
+    </script>
+    @endpush
+    @endif
+
+
+    <div class="col-12 mb-2">
+        <div class="card border-light shadow-sm components-section">
+            <div class="card-body">
+                <div class="row">
+
+                    <div class="table-responsive">
+                        <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Image</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('addon-script')
+<script>
+    // AJAX DataTable
+    var datatable = $('#crudTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        ajax: {
+            url: '{!! url()->current() !!}',
+        },
+        columns: [{
+                data: 'id',
+                name: 'id'
+            },
+            {
+                data: 'name',
+                name: 'name'
+            },
+            {
+                data: 'email',
+                name: 'email'
+            },
+            {
+                data: 'image',
+                name: 'image'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false,
+                width: '15%'
+            },
+        ]
+    });
+
+</script>
+@endpush
